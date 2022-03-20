81b7ef08120352b65088c821ceb9963d77bad6d3
limjungyeob <ehdtks1526@gmail.com>
2022-03-20 16:39:59 +0900

    main

---
+++ PminQ2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PminQ2 {
+    static int min(int a, int b, int c){
+        int min=a;
+        if(b<min) min = b;
+        if(c<min) min = c;
+        return min;
+    }
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("a의 값 : "); int a = stdIn.nextInt();
+        System.out.print("b의 값 : "); int b = stdIn.nextInt();
+        System.out.print("c의 값 : "); int c = stdIn.nextInt();
+        System.out.println("최솟값은 : "+ min(a,b,c));
+
+    }
+}
