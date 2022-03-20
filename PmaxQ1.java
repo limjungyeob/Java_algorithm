a190c77da2334c857a4a44862cc98356ca4ae8af
limjungyeob <ehdtks1526@gmail.com>
2022-03-20 16:34:06 +0900

    main

---
+++ PmaxQ1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PmaxQ1 {
+    static int max4(int a, int b, int c, int d){
+        int max=a;
+        if(b>max) max=b;
+        if(c>max) max =c;
+        if(d>max) max =d;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.print("a의 값 : "); int a = stdIn.nextInt();
+        System.out.print("b의 값 : "); int b = stdIn.nextInt();
+        System.out.print("c의 값 : "); int c = stdIn.nextInt();
+        System.out.print("d의 값 : "); int d = stdIn.nextInt();
+        System.out.println("최댓값은: "+max4(a, b, c, d));
+    }
+}
