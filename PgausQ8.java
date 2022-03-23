4c5f42e3f7fa1eef8d70123842d236b880cbc21b
limjungyeob <ehdtks1526@gmail.com>
2022-03-23 13:57:00 +0900

    main

---
+++ PgausQ8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PgausQ8 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.print("n의값: "); int n = stdIn.nextInt();
+        int sum= (n+1)*(n/2);
+        if(n%2!=0){
+            sum+=(n+1)/2;
+        }
+        System.out.println("합은"+sum);
+    }
+}
