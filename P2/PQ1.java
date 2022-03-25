a81d7d587cfc478e4120d2230ce870016b2e973f
limjungyeob <ehdtks1526@gmail.com>
2022-03-25 16:54:00 +0900

    main

---
+++ P2/PQ1.java
@@ -0,0 +1,29 @@
+package P2;
+
+import java.util.Random;
+
+public class PQ1 {
+    static int maxOf(int[] a){
+        int max=a[0];
+        for(int i=0;i<a.length;i++){
+            
+            if(a[i]>max){
+                max=a[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int PersonNum = rand.nextInt(10);
+
+        int[] height = new int[PersonNum];
+        System.out.println("사람수는 : "+PersonNum);
+        for(int i=0; i<PersonNum;i++){
+            height[i]= 100+rand.nextInt(90);
+            System.out.println("height{" + i + "]:"+height[i]);
+        }
+
+        System.out.println("최댓값은"+maxOf(height)+"입니다.");
+    }
+}
---
+++ P2/PQ2.java
@@ -0,0 +1,46 @@
+package P2;
+
+import java.util.Scanner;
+
+public class PQ2 {
+    static void swap(int[] a,int idx1, int idx2){
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+    
+    static void reverse(int[] a){
+        for(int i=0;i<a.length/2;i++){
+            for(int j=0;j<a.length;j++){
+                System.out.print(a[j]+" ");
+            }
+            System.out.println();
+            System.out.println("a[" + i +"]과(와) a["+(a.length -i -1)+"]를 교환합니다.");;
+            swap(a, i, a.length -i -1);
+           
+
+        }
+        System.out.println("역순 정렬을 마쳤습니다.");
+    }
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("요솟수: ");
+        int num = stdIn.nextInt();
+
+        int[] x = new int[num];
+
+        for(int i=0; i<num;i++)
+        {
+            System.out.print("x["+i+"] : ");
+            x[i]=stdIn.nextInt();
+        }
+
+        reverse(x);
+
+       
+
+        
+    }
+    }
+
