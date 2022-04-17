b188bebce4cb3f934e02e059cdb70d9f13fce6ae
limjungyeob <ehdtks1526@gmail.com>
2022-04-17 16:56:29 +0900

    main

--- P4/PQ3.java
+++ P4/PQ3.java
@@ -3,70 +3,140 @@ package P4;
 import java.util.Scanner;
 
 public class PQ3 {
+    static String a;
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
         Q3Stack s = new Q3Stack(64);
         try {
            while(true){
-            System.out.println("현재 데이터 수 :"+s.size()+"/"+s.capacity());
+            System.out.println("현재 A데이터 수 :"+s.sizeA()+"/"+s.capacity()+"현재 B데이터 수:"+s.sizeB()+"/"+s.capacity());
 
             System.out.print("(1)푸시 (2)팝 (3)피크  "+"(4)덤프 (5)탐색 (6)초기화 :"+ "(7)index (0)종료: ");
 
             int menu = stdIn.nextInt();
             if(menu==0) break;
-
+            
+            String A = "a";
+            String B = "b";
             int x;
             switch(menu){
                 case 1: //푸시
-                System.out.print("데이터 :");
-                x = stdIn.nextInt();
-                try {
-                    s.push(x);
-                } catch (IntStack.OverflowIntStackException e) {
-                    System.out.println("스택이 가득 찼습니다.");
-                }
-                break;
+                    
+                    System.out.print("a스택과 b스택중 선택하세요:");
+                    a=stdIn.next();
+                    if(a.equals(A)){
+                        System.out.print("데이터 :");
+                        x = stdIn.nextInt();
+                        try {
+                            s.pushA(x);
+                        } catch (IntStack.OverflowIntStackException e) {
+                            System.out.println("스택이 가득 찼습니다.");
+                        }
+                        break;
+                    }else if(a.equals(B)){
+                        System.out.print("데이터 :");
+                        x = stdIn.nextInt();
+                        try {
+                            s.pushB(x);
+                        } catch (IntStack.OverflowIntStackException e) {
+                            System.out.println("스택이 가득 찼습니다.");
+                        }
+                        break;
+                    }else{
+                        break;
+                    }
 
                 case 2: //팝
-                try {
-                    x=s.pop();
-                    System.out.println("팝한 데이터는 "+x+"입니다.");
-                } catch (IntStack.EmptyIntStackException e) {
-                    System.out.println("스택이 비어 있습니다.");
-                }
-                break;
-
+                    
+                    System.out.print("a스택과 b스택중 선택하세요.");
+                    a=stdIn.next();
+                    if(a.equals(A)){
+                            try {
+                                s.popA();
+                                
+                            } catch (IntStack.OverflowIntStackException e) {
+                            System.out.println("스택이 가득 찼습니다.");
+                        
+                            }
+                        break;
+                    }else if(a.equals(B)){
+                            try {
+                                s.popB();
+                            } catch (IntStack.OverflowIntStackException e) {
+                                System.out.println("스택이 가득 찼습니다.");
+                            }
+                        break;
+                    }else{
+                        break;
+                    }
                 case 3: //피크
-                try {
-                    x=s.peek();
-                    System.out.println("피크한 데이터는"+x+"입니다.");
-                } catch (IntStack.EmptyIntStackException e) {
-                    System.out.println("스택이 비어 있습니다.");
-                }
-                break;
+                    System.out.print("a스택과 b스택중 선택하세요.");
+                    a=stdIn.next();
+                    if(a.equals(A)){
+                        try {
+                            x=s.peekA();
+                            System.out.println("피크한 데이터는"+x+"입니다.");
+                        } catch (IntStack.OverflowIntStackException e) {
+                        System.out.println("스택이 가득 찼습니다.");
+                    
+                        }
+                        break;
+                    }else if(a.equals(B)){
+                        try {
+                            x=s.peekB();
+                            System.out.println("피크한 데이터는"+x+"입니다.");
+                        } catch (IntStack.OverflowIntStackException e) {
+                            System.out.println("스택이 가득 찼습니다.");
+                        }
+                        break;
+                    }else{
+                        break;
+                    }
+                
 
                 case 4: //덤프
-                    s.dump();
+                    s.dumpA();
+                    s.dumpB();
                     break;
 
                 case 5: //탐색
+                System.out.print("a스택과 b스택중 선택하세요.");
+                a=stdIn.next();
+                if(a.equals(A)){
                     System.out.print("검색할 데이터: ");
                     x=stdIn.nextInt();
-                    int n = s.indexOf(x);
+                    int n = s.indexOfA(x);
                     if(n >= 0){
-                        System.out.println("검색할 데이터는 "+(s.size()-n)+"번째 있습니다.");
+                        System.out.println("검색할 데이터는 "+(s.sizeA()-n)+"번째 있습니다.");
                     }else{
                         System.out.println("그 데이터는 없습니다.");
                     }
                     break;
+                }else if(a.equals(B)){
+                    System.out.print("검색할 데이터: ");
+                    x=stdIn.nextInt();
+                    int n = s.indexOfB(x);
+                    if(n >= 0){
+                        System.out.println("검색할 데이터는 "+(s.sizeB()-n)+"번째 있습니다.");
+                    }else{
+                        System.out.println("그 데이터는 없습니다.");
+                    }
+                    break;
+                }else{
+                    break;
+                }
                 case 6: //clear
                     s.clear();
                     break;
                 case 7: //index
-                    System.out.println("용량: "+s.capacity());
-                    System.out.println("데이터 수: "+s.size());
-                    System.out.println("비어"+(s.isEmpty()?"있습니다.":"있지 않습니다."));
-                    System.out.println("꽉차"+(s.isFull()?"있습니다.":"있지 않습니다."));
+                    System.out.println("A용량: "+s.capacity());
+                    System.out.println("A데이터 수: "+s.sizeA());
+                    System.out.println("A는비어"+(s.isEmptyA()?"있습니다.":"있지 않습니다."));
+                    System.out.println("A는꽉차"+(s.isFullA()?"있습니다.":"있지 않습니다."));
+                    System.out.println("B용량: "+s.capacity());
+                    System.out.println("B데이터 수: "+s.sizeB());
+                    System.out.println("B는비어"+(s.isEmptyB()?"있습니다.":"있지 않습니다."));
+                    System.out.println("B는꽉차"+(s.isFullB()?"있습니다.":"있지 않습니다."));
                     break;
             }
             
--- P4/Q3Stack.java
+++ P4/Q3Stack.java
@@ -2,7 +2,8 @@ package P4;
 
 public class Q3Stack {
     private int max;    //스택 용량
-    private int ptr;    //스택 포인터
+    private int ptrA;    //스택 포인터A
+    private int ptrB;   //스택 포인터 B
     private int[] stk;  //스택 본체
 
     // 실행 시 예외: 스택이 비어 있음
@@ -19,8 +20,10 @@ public class Q3Stack {
 
     //생성자
     public Q3Stack(int capacity){
-        ptr = 0;        //생성 시 스택은 비어 있으므로 스택 포인터 ptr값을 0으로 한다.
+        
         max = capacity;
+        ptrA=0; //A스택 포인터 초기화
+        ptrB=max-1; //B스택 포인터 초기화
         try{
             stk = new int[max];     //스택 본체용 배열을 생성
         }catch(OutOfMemoryError e){  //생성할 수 없음
@@ -28,68 +31,116 @@ public class Q3Stack {
         }
     }
 
-    //스택에 x를 푸쉬
-    public int push(int x) throws OverflowQ3StackException{
-        if(ptr>=max)        //스택이 가득참
+    //스택A에 x를 푸쉬
+    public int pushA(int x) throws OverflowQ3StackException{
+        if(ptrA>ptrB)        //스택이 가득참
             throw new OverflowQ3StackException();
-        return stk[ptr++] = x;
+        return stk[ptrA++] = x;
+    }
+    //스택 B에 x를 푸쉬
+    public int pushB(int x) throws OverflowQ3StackException{
+        if(ptrB<ptrA)  //스택이 가득참
+            throw new OverflowQ3StackException();
+        return stk[ptrB--] = x;
     }
 
-    //스택에서 데이터를 팝(정상에 있는 데이터를 꺼냄)
-    public int pop() throws EmptyQ3StackException{
-        if(ptr <= 0) //스택이 비어 있음
+    //A스택에서 데이터를 팝(정상에 있는 데이터를 꺼냄)
+    public int popA() throws EmptyQ3StackException{
+        if(ptrA <= 0) //스택이 비어 있음
             throw new EmptyQ3StackException();
-        return stk[--ptr];
+        return stk[--ptrA];
+    }
+    //B스택에서 데이터를 팝(정상에 있는 데이터를 꺼냄)
+    public int popB() throws EmptyQ3StackException{
+        if(ptrB<max/2)
+            throw new EmptyQ3StackException();
+        return stk[++ptrB];
     }
 
-    //스택에서 데이터를 피크(정상에 있는 데이터를 들여다봄)
-    public int peek() throws EmptyQ3StackException{
-        if(ptr <= 0)    //스택이 비어 있음
+    //A스택에서 데이터를 피크(정상에 있는 데이터를 들여다봄)
+    public int peekA() throws EmptyQ3StackException{
+        if(ptrA <= 0)    //스택이 비어 있음
             throw new EmptyQ3StackException();
-        return stk[ptr-1];
+        return stk[ptrA-1];
+    }
+
+    //B스택에서 데이터를 피크 (정상에 있는 데이터를 들여다봄)
+    public int peekB() throws EmptyQ3StackException{
+        if(ptrB == max-1)//스택이 비어있음
+            throw new EmptyQ3StackException();
+        return stk[ptrB+1];
     }
 
     //스택에서 x를 찾아 인덱스(없으면 -1)를 반환
-    public int indexOf(int x){
-        for(int i=ptr-1;i>=0;i--){   //정상쪽에서 선형검색
+    public int indexOfA(int x){
+        for(int i=ptrA-1;i>=0;i--){   //정상쪽에서 선형검색
             if(stk[i] == x){
                 return i; //검색성공  
             }
         }
          return -1;  //검색실패
     }
+    public int indexOfB(int x){
+        for(int i=ptrB+1;i<max;i++){ //정상쪽에서 선형검색
+            if(stk[i] == x){
+                return i;
+            }
+        }
+        return -1;
+    }
 
     //스택을 비움
     public void clear(){
-        ptr = 0;
+        ptrA = 0;
+        ptrB = max-1;
     }
 
     //스택의 용량을 반환
     public int capacity(){
-        return max;
+        return max/2;
     }
 
-    //스택에 쌓여 있는 데이터 수를 반환
-    public int size(){
-        return ptr;
+    //스택에 쌓여 있는 A데이터 수를 반환
+    public int sizeA(){
+        return ptrA;
     }
 
+    //스택에 쌓여 있는 B데이터 수를 반환
+    public int sizeB(){
+        return max-ptrB-1;
+    }
     //스택이 비어 있는가?
-    public boolean isEmpty(){
-        return ptr<=0;
+    public boolean isEmptyA(){
+        return ptrA<=0;
+    }
+
+    public boolean isEmptyB(){
+        return ptrB==max-1;
     }
 
     //스택이 가득 찼는가?
-    public boolean isFull(){
-        return ptr >= max;
+    public boolean isFullA(){
+        return ptrA >= max-1/2;
+    }
+    public boolean isFullB(){
+        return ptrB <= max-1/2;
     }
 
     // 스택 안의 모든 데이터를 바닥 -> 꼭대기 순서로 출력
-    public void dump(){
-        if(ptr <= 0){
+    public void dumpA(){
+        if(ptrA <= 0){
             System.out.println("스택이 비어 있습니다.");
         }else{
-            for(int i=0; i < ptr;i++)
+            for(int i=0; i < ptrA;i++)
+                System.out.print(stk[i]+"");
+            System.out.println();
+        }
+    }
+    public void dumpB(){
+        if(ptrB == max-1){
+            System.out.println("스택이 비어 있습니다.");
+        }else{
+            for(int i=ptrB+1; i<max-1;i++)
                 System.out.print(stk[i]+"");
             System.out.println();
         }
