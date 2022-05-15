a1a9390dc5d75680c27c30eea47d974e4b889444
limjungyeob <ehdtks1526@gmail.com>
2022-05-15 17:49:40 +0900

    main

---
+++ P7/IntSetTester1.java
@@ -0,0 +1,10 @@
+package P7;
+
+public class IntSetTester1 {
+    public static void main(String[] args) {
+        IntSetPlus1 s1 = new IntSetPlus1(20);
+        
+
+        System.out.println("s1은"+s1.isEmpty1());
+    }
+}
