5bee3d637b58ff876ed071b8b8d6738b327bc364
limjungyeob <ehdtks1526@gmail.com>
2022-05-15 17:49:45 +0900

    main

--- P7/IntSetTester1.java
+++ P7/IntSetTester1.java
@@ -5,6 +5,6 @@ public class IntSetTester1 {
         IntSetPlus1 s1 = new IntSetPlus1(20);
         
 
-        System.out.println("s1은"+s1.isEmpty1());
+        System.out.println("s1은"+(s1.isEmpty()?"비었습니다.":"비어있지 않습니다."));
     }
 }
